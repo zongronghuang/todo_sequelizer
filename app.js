eda9816e5033e323e7324d99e82e1cfd78be0e3e
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-03-28 23:01:03 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const port = 3000
+
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+// 認證系統的路由
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(user => res.redirect('/'))
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+app.listen(port, () => {
+  console.log(`Server up and running at http://localhost:${port}`)
+})
